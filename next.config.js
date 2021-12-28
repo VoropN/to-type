c2e85122b4bec3286a5629e19078d9ada40599a5
Mykola Voropinov <voropn@gmail.com>
2021-12-28 20:10:49 +0200

    fix confix options

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 module.exports = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'components'), path.join(__dirname, 'components/Timer')],
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({ test: /\.txt$/, use: 'raw-loader' });
 
