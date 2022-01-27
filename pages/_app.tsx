0cbcff23e9a181d56d1ac31c2b4b919d069f4d7d
voropn <voropn@gmail.com>
2022-01-27 22:40:42 +0200

    rename variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Amplify from 'aws-amplify';
-import config from 'src/aws-exports.js';
+import config from 'src/aws-exports';
 
 // Amplify SSR configuration needs to be done within each API route
 Amplify.configure({ ...config, ssr: true });
