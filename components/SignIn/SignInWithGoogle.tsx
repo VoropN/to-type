d6a74112e016713d6b312bce003c4b9f5355c2f0
voropn <voropn@gmail.com>
2022-01-29 13:58:39 +0200

    add google login

--- components/SignIn/SignInWithGoogle.tsx
+++ components/SignIn/SignInWithGoogle.tsx
@@ -1,29 +1,71 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { Auth } from 'aws-amplify';
-// To federated sign in from Google
+
+declare global {
+  interface Window {
+    gapi: any;
+  }
+}
+
 const SignInWithGoogle = () => {
+  const [ga, setGa] = useState();
+
+  const initGapi = () => {
+    // init the Google SDK client
+    setGa(window.gapi);
+    const g = window.gapi;
+    g.load('auth2', function () {
+      g.auth2.init({
+        client_id:
+          '533677469724-4q8glcedf5hm5pl8g385nvspgtsn8p2a.apps.googleusercontent.com',
+        // authorized scopes
+        scope: 'profile email openid',
+      });
+    });
+  };
+
+  const createScript = () => {
+    // load the Google SDK
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/platform.js';
+    script.async = true;
+    script.onload = initGapi;
+    document.body.appendChild(script);
+  };
+
   useEffect(() => {
-    const ga =
-      window.gapi && window.gapi.auth2
-        ? window.gapi.auth2.getAuthInstance()
-        : null;
+    if (!ga) return;
+    window.gapi.signin2.render('gs2', {
+      scope: 'email',
+      width: 150,
+      height: 25,
+      longtitle: true,
+      theme: 'dark',
+      // onsuccess: onGoogleSignIn,
+    });
+  }, [ga]);
+
+  useEffect(() => {
+    const ga = window?.gapi?.auth2?.getAuthInstance?.() || null;
 
     if (!ga) createScript();
   }, []);
 
   const signIn = () => {
-    const ga = window.gapi.auth2.getAuthInstance();
-    ga.signIn().then(
-      (googleUser) => {
-        getAWSCredentials(googleUser);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    window.gapi?.auth2
+      .getAuthInstance()
+      ?.signIn()
+      .then(
+        (googleUser: any) => {
+          getAWSCredentials(googleUser);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
   };
 
-  const getAWSCredentials = async (googleUser) => {
+  const getAWSCredentials = async (googleUser: any) => {
     const { id_token, expires_at } = googleUser.getAuthResponse();
     const profile = googleUser.getBasicProfile();
     let user = {
@@ -39,33 +81,9 @@ const SignInWithGoogle = () => {
     console.log('credentials', credentials);
   };
 
-  const createScript = () => {
-    // load the Google SDK
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/platform.js';
-    script.async = true;
-    script.onload = initGapi;
-    document.body.appendChild(script);
-  };
-
-  const initGapi = () => {
-    // init the Google SDK client
-    const g = window.gapi;
-    g.load('auth2', function () {
-      g.auth2.init({
-        client_id:
-          '925734069085-9084044v6bl6kdg0hqn6oq9aspqtlndq.apps.googleusercontent.com',
-        // authorized scopes
-        scope: 'profile email openid',
-      });
-    });
-  };
-
   return (
-    <div>
-      <button className="g-signin2" onClick={signIn}>
-        Sign in with Google
-      </button>
+    <div id="gs2" onClick={signIn} style={{ marginTop: 5 }}>
+      Sign in with Google
     </div>
   );
 };
