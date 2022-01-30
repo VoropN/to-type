b0fa3e225751f906121161186b1afae162c014b4
voropn <voropn@gmail.com>
2022-01-30 17:01:45 +0200

    update google login

--- components/Profile/_.tsx
+++ components/Profile/_.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss';
 import classNames from 'classnames';
 import { Button } from '@mui/material';
 import { Auth } from 'aws-amplify';
+import SignInWithGoogle from 'components/SignIn/SignInWithGoogle';
 
 const Profile = () => {
   const [user, setUser] = useState<null | { email: string }>(null);
@@ -19,6 +20,8 @@ const Profile = () => {
   };
   return (
     <div className={classNames(styles.container)}>
+      <SignInWithGoogle />
+
       {user ? (
         <>
           <div className={styles.userData}>{user.email}</div>
--- components/SignIn/SignInWithGoogle.tsx
+++ components/SignIn/SignInWithGoogle.tsx
@@ -1,91 +1,54 @@
 import React, { memo, useEffect, useState } from 'react';
-import { Auth } from 'aws-amplify';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+import awsConfig from 'src/aws-exports';
 
-declare global {
-  interface Window {
-    gapi: any;
-  }
-}
+Amplify.configure(awsConfig);
 
-const SignInWithGoogle = () => {
-  const [ga, setGa] = useState();
-
-  const initGapi = () => {
-    // init the Google SDK client
-    setGa(window.gapi);
-    const g = window.gapi;
-    g.load('auth2', function () {
-      g.auth2.init({
-        client_id:
-          '533677469724-4q8glcedf5hm5pl8g385nvspgtsn8p2a.apps.googleusercontent.com',
-        // authorized scopes
-        scope: 'profile email openid',
-      });
-    });
-  };
-
-  const createScript = () => {
-    // load the Google SDK
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/platform.js';
-    script.async = true;
-    script.onload = initGapi;
-    document.body.appendChild(script);
-  };
+const Login = () => {
+  const [user, setUser] = useState<any>(null);
+  const [customState, setCustomState] = useState(null);
 
   useEffect(() => {
-    if (!ga) return;
-    window.gapi.signin2.render('gs2', {
-      scope: 'email',
-      width: 150,
-      height: 25,
-      longtitle: true,
-      theme: 'dark',
-      // onsuccess: onGoogleSignIn,
+    const unsubscribe = Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+          setUser(data);
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'customOAuthState':
+          setCustomState(data);
+      }
     });
-  }, [ga]);
 
-  useEffect(() => {
-    const ga = window?.gapi?.auth2?.getAuthInstance?.() || null;
+    Auth.currentAuthenticatedUser()
+      .then((currentUser) => setUser(currentUser))
+      .catch(() => console.log('Not signed in'));
 
-    if (!ga) createScript();
+    return unsubscribe;
   }, []);
 
-  const signIn = () => {
-    window.gapi?.auth2
-      .getAuthInstance()
-      ?.signIn()
-      .then(
-        (googleUser: any) => {
-          getAWSCredentials(googleUser);
-        },
-        (error: any) => {
-          console.log(error);
-        }
-      );
-  };
-
-  const getAWSCredentials = async (googleUser: any) => {
-    const { id_token, expires_at } = googleUser.getAuthResponse();
-    const profile = googleUser.getBasicProfile();
-    let user = {
-      email: profile.getEmail(),
-      name: profile.getName(),
-    };
-
-    const credentials = await Auth.federatedSignIn(
-      'google',
-      { token: id_token, expires_at },
-      user
-    );
-    console.log('credentials', credentials);
-  };
-
   return (
-    <div id="gs2" onClick={signIn} style={{ marginTop: 5 }}>
-      Sign in with Google
+    <div className="App">
+      <button
+        onClick={() =>
+          Auth.federatedSignIn({
+            provider: CognitoHostedUIIdentityProvider.Google,
+          })
+        }
+      >
+        Open Google
+      </button>
+      <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+      {user && (
+        <button onClick={() => Auth.signOut()}>
+          Sign Out {user.getUsername()}
+        </button>
+      )}
     </div>
   );
 };
 
-export default memo(SignInWithGoogle);
+export default memo(Login);
